ea168ad41e27f8bf7d2308da422b8a0b3202086e
Scott Mason <smason@oes.com>
2024-01-09 10:58:07 +0000

    Initial commit (by create-cloudflare CLI)

---
+++ src/worker.js
@@ -0,0 +1,26 @@
+export default {
+
+  async fetch(request) {
+
+    class ElementHandler {
+      element(element) {
+        const fact = getFact();
+        element.append(`<div>${fact}</div>`, {html: true});
+      }
+    }
+
+    const res = await fetch(request);
+    return new HTMLRewriter().on('section#content', new ElementHandler()).transform(res);
+
+
+    async function getFact() {
+      const response = await fetch(`https://catfact.ninja/fact`);
+      try {
+        const result = await response.json();
+        return result.fact;
+      } catch (err) {
+        return err;
+      }
+    }
+  }
+}
